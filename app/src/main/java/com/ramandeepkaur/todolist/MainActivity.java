287804624d28a884dc154f0aeb75e113b3aee876
raman2021 <ramandeepsandhu9882@gmail.com>
2021-11-12 16:47:04 -0500

    comments

--- app/src/main/java/com/ramandeepkaur/todolist/MainActivity.java
+++ app/src/main/java/com/ramandeepkaur/todolist/MainActivity.java
@@ -18,25 +18,28 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
-
+//default package and super class
 public class MainActivity extends AppCompatActivity {
+    // decrlearing some variables
     List<String> todoList;
     ArrayAdapter<String> arrayAdapter;
     ListView listView;
 
     EditText editText;
-
+//using override to recall super class
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+// adding variable todolist to declare arrays
         todoList = new ArrayList<>();
         arrayAdapter = new ArrayAdapter<>(this,R.layout.list_view_layout,todoList);
         listView = findViewById(R.id.id_list_view);
-
+//adding method to make button
+// working when button is clicked
         listView.setAdapter(arrayAdapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            //calling super class method in textview
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 TextView textView = (TextView) view;
@@ -45,8 +48,10 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        //giving id to find and display it
         editText = findViewById(R.id.id_edit_text);
     }
+    //adding method to add item in list when user enter something in keyboard to add
     public void addItemToList(View view){
 
         todoList.add(editText.getText().toString());
